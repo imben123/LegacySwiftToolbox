97752b114a6acb6a477473fd9e43fa26954a5dff
Ben Davis <ben@flypay.co.uk>
2016-12-26 18:14:39 +0000

    Added xcode project

---
+++ SwiftToolbox/SwiftToolbox/SwiftToolbox.h
@@ -0,0 +1,19 @@
+//
+//  SwiftToolbox.h
+//  SwiftToolbox
+//
+//  Created by Ben Davis on 26/12/2016.
+//  Copyright © 2016 bendavisapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftToolbox.
+FOUNDATION_EXPORT double SwiftToolboxVersionNumber;
+
+//! Project version string for SwiftToolbox.
+FOUNDATION_EXPORT const unsigned char SwiftToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftToolbox/PublicHeader.h>
+
+
