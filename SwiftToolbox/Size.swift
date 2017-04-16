2ad498fd3d87b52c72b81f9f037e4826503e2a0c
Ben Davis <ben@flypay.co.uk>
2017-04-16 23:36:52 +0100

    Size multiplication

---
+++ SwiftToolbox/Size.swift
@@ -0,0 +1,17 @@
+//
+//  Size.swift
+//  SwiftToolbox
+//
+//  Created by Ben Davis on 16/04/2017.
+//  Copyright © 2017 bendavisapps. All rights reserved.
+//
+
+import Foundation
+
+public extension CGSize {
+    
+    public static func *(_ lhs: CGSize, rhs: CGFloat) -> CGSize {
+        return CGSize(width: lhs.width*rhs, height: lhs.height*rhs)
+    }
+    
+}
